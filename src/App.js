b966cab3c76af6893a2dd8a48e5e3d03ade7465a
Vincent Yeh <difkye247@gmail.com>
2024-05-20 11:14:39 +0800

    add card compared success logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const card = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
 ];
 function App() {
   let [cardSrc, setCardSrc] = useState([]);
   let [turn, setTurn] = useState(0);
+  let [chooseOne, setChooseOne] = useState(null);
+  let [chooseTwo, setChooseTwo] = useState(null);
+
+  console.log(cardSrc);
+  // validate the two choice state and increase Turn
+  useEffect(() => {
+    if (chooseOne && chooseTwo) {
+      if (chooseOne.src === chooseTwo.src) {
+        console.log("compare true");
+        resetChoice();
+        setCardSrc((previous) => {
+          return previous.map((card) => {
+            if (chooseOne.src === card.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        setTurn((previousTurn) => previousTurn + 1);
+      } else {
+        console.log("compare false");
+        resetChoice();
+      }
+    }
+  }, [chooseOne, chooseTwo]);
+
+  // handle choice
+  const chooseCard = (card) => {
+    // console.log(card);
+    chooseOne ? setChooseTwo(card) : setChooseOne(card);
+  };
+
+  // reset Choice
+  const resetChoice = () => {
+    setChooseOne(null);
+    setChooseTwo(null);
+  };
 
   // shuffle cards
   const shuffleCard = () => {
@@ -26,7 +64,8 @@ function App() {
     setCardSrc(shuffledCard);
     setTurn(0);
   };
-  console.log(cardSrc);
+  // console.log(cardSrc);
+
   return (
     <div className="App">
       <h1>Magic Memory</h1>
@@ -39,7 +78,9 @@ function App() {
       </button>
       <div className="card-grid">
         {cardSrc.map((card) => {
-          return <SingleCard key={card.id} src={card.src} />;
+          return (
+            <SingleCard chooseCard={chooseCard} key={card.id} card={card} />
+          );
         })}
       </div>
     </div>
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./SingleCard.css";
 export default function SingleCard(props) {
-  let { src } = props;
+  let { card, chooseCard } = props;
+  const choose = () => {
+    chooseCard(card);
+  };
   return (
     <div>
       <div className="card">
-        <img className="font" alt="card-font" src={src} />
-        <img className="back" src="/img/cover.png" alt="" />
+        <img className="font" alt="card-font" src={card.src} />
+        <img onClick={choose} className="back" src="/img/cover.png" alt="" />
       </div>
     </div>
   );
